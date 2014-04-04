a4bb2ffa3f5f82d31d2bcd1221633bd8ea536180
Yogi <yogendra.ogn@gmail.com>
2014-04-04 20:17:56 +0530

    4april app

---
+++ controllers/tasks_controller.rb
@@ -0,0 +1,68 @@
+class TasksController < ApplicationController
+	before_filter :authenticate_user!
+	def index
+		if params[:with] =="my_task"
+		  @tasks = current_user.tasks.paginate(:page => params[:page], :per_page => 3).search(params[:search])
+	  elsif params[:with] =="upcoming_task"
+	  	@tasks = Task.find_by_task_status("Pending")
+    elsif params[:with] =="completed_task"
+	  	@tasks = Task.find_by_task_status("Working")
+	  elsif params[:with] == "project_tasks"
+	  	@project = Project.find(params[:project_id])
+	  	@tasks = @project.tasks.paginate(:page => params[:page], :per_page => 3).search(params[:search])
+		else
+			@tasks = Task.paginate(:page => params[:page], :per_page => 3).search(params[:search])
+    end
+    @user = User.all
+    @tasks = Task.paginate(:page => params[:page], :per_page => 3).search(params[:search])
+    respond_to do |format|
+    	format.js
+    	format.html
+    end
+	end
+
+	def new
+		@task = Task.new
+		# respond_to do |format|
+  #     format.js
+  #   end
+	end
+
+	def show
+		@task = Task.find(params[:id])
+	end
+
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def create
+		debugger
+		@project = Project.find(params[:task][:project_id])
+		@task = @project.tasks.build(params[:task])
+		@task = current_user.tasks.build(params[:task])
+		if @task.save
+			redirect_to tasks_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		if @task.update_attributes(:task)
+			redirect_to task_path(@task)
+		else
+			render 'index'
+		end
+	end
+
+	def destroy
+		@task = Task.find(params[:id])
+		if @task.delete
+			redirect_to tasks_path
+		else
+			render 'index'
+		end
+	end
+end
---
+++ models/task.rb
@@ -0,0 +1,20 @@
+class Task < ActiveRecord::Base
+  attr_accessible :all_project, :ends_on, :project_id, :starts_on, :task_created_by, :task_created_on, :task_details, :title, :user_id, :task_status
+  belongs_to :project
+  belongs_to :user
+
+  has_many :photos, as: :imageable
+  accepts_nested_attributes_for :photos
+
+  has_many :task_comments
+  accepts_nested_attributes_for :task_comments
+
+  ####Simple Search#####
+	def self.search(search)
+	  if search
+	    find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+	  else
+	    find(:all)
+	  end
+	end
+end
